3955bdc4afd816058ef4934994e7a5960770e68a
Chen <2934570984@qq.com>
2024-05-11 03:27:13 +0800

    feat: 新增问卷提交成功&失败页

---
+++ src/pages/fail.tsx
@@ -0,0 +1,8 @@
+export default function Fail() {
+  return (
+    <div>
+      <h1>失败</h1>
+      <p>问卷提交失败</p>
+    </div>
+  );
+}
---
+++ src/pages/success.tsx
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div>
+      <h1>成功</h1>
+      <p>问卷提交成功</p>
+    </div>
+  );
+}
