e4edc4baf7bfaa6bc9b2cd0f4f6baf64f124b822
Chen <2934570984@qq.com>
2024-05-11 14:54:29 +0800

    feat: 获取问卷数据，并校验是否正常

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -4,18 +4,70 @@ import QuestionInput from "@/components/QuestionComponents/QuestionInput";
 import QuestionRadio from "@/components/QuestionComponents/QuestionRadio";
 import styles from "@/styles/Question.module.scss";
 import PageWrapper from "@/components/PageWrapper";
+import { getQuestionById } from "@/services/question";
 
 const inter = Inter({ subsets: ["latin"] });
 
 type PropsType = {
-  id: string;
+  errno: number;
+  data?: {
+    id: string;
+    title: string;
+    desc?: string;
+    js?: string;
+    css?: string;
+    isPublished: boolean;
+    isDeleted: boolean;
+    componentList: Array<any>;
+  };
+  msg?: string;
 };
 
 export default function Question(props: PropsType) {
+  const { errno, data, msg = "" } = props;
+
+  // 数据错误
+  if (errno !== 0) {
+    return (
+      <PageWrapper title="错误">
+        <h1>错误</h1>
+        <p>{msg}</p>
+      </PageWrapper>
+    );
+  }
+
+  const {
+    id,
+    title = "",
+    desc = "",
+    isDeleted,
+    isPublished,
+    componentList = [],
+  } = data || {};
+
+  // 已经被删除的，提示错误
+  if (isDeleted) {
+    return (
+      <PageWrapper title={title} desc={desc}>
+        <h1>{title}</h1>
+        <p>该问卷已经被删除</p>
+      </PageWrapper>
+    );
+  }
+
+  // 尚未发布的，提示错误
+  if (!isPublished) {
+    return (
+      <PageWrapper title={title} desc={desc}>
+        <h1>{title}</h1>
+        <p>该问卷尚未发布</p>
+      </PageWrapper>
+    );
+  }
   return (
-    <PageWrapper title="question">
+    <PageWrapper title={title} desc={desc}>
       <form method="post" action="/api/answer">
-        <input type="hidden" name="questionId" value={props.id} />
+        <input type="hidden" name="questionId" value={id} />
         <div className={styles.componentWrapper}>
           <QuestionInput
             fe_id="c1"
@@ -47,11 +99,12 @@ export default function Question(props: PropsType) {
 }
 
 export async function getServerSideProps(context: any) {
-  const { id = "id" } = context.params;
+  const { id = "" } = context.params;
+
+  // 根据 id 获取问卷数据
+  const data = await getQuestionById(id);
 
   return {
-    props: {
-      id,
-    },
+    props: data,
   };
 }
